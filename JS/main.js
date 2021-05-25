6a87e95071e80d4273affd0344635cdc876ae9a3
Jamie De Mooij <302507648@student.rocmondriaan.nl>
2021-05-25 16:38:22 +0200

    game total

--- JS/main.js
+++ JS/main.js
@@ -4,16 +4,23 @@ class Player
         this.name = name;
         this.symbol = symbol;
         this.points = 0;
+        this.games = 1;
     }
     addPoints(givePoints){
         this.points += givePoints;
     }
+    addGames(){
+        this.games += 1;
+    }
 }
 const playerOne = new Player("Henk","X");
-console.log(playerOne);
-console.log(playerOne.name);
+//console.log(playerOne);
+//console.log(playerOne.name);
 const playerTwo = new Player("Nathan", "O");
-console.log(playerTwo.symbol);
-console.log(playerOne.points);
+//console.log(playerTwo.symbol);
+//console.log(playerOne.points);
 playerOne.addPoints(69);
-console.log(playerOne.points);
+//console.log(playerOne.points);
+console.log("Total games: " + playerOne.games);
+playerOne.addGames();
+console.log("Total games: " + playerOne.games);
